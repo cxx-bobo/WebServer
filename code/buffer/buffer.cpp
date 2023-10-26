f79717d7eb37a5112a8b4ff997b58a16c8ba9744
cxx-bobo <986526658@qq.com>
2023-10-26 13:13:32 +0000

    buffer

---
+++ code/buffer/buffer.cpp
@@ -0,0 +1,161 @@
+#include "buffer.h"
+
+// 读写下标初始化，vector<char>初始化
+Buffer::Buffer(int initBuffSize) : buffer_(initBuffSize), readPos_(0), writePos_(0)  {}  
+
+// 可写的数量：buffer大小 - 写下标
+size_t Buffer::WritableBytes() const {
+    return buffer_.size() - writePos_;
+}
+
+// 可读的数量：写下标 - 读下标
+size_t Buffer::ReadableBytes() const {
+    return writePos_ - readPos_;
+}
+
+// 可预留空间：已经读过的就没用了，等于读下标
+size_t Buffer::PrependableBytes() const {
+    return readPos_;
+}
+
+// 返回缓冲区中可读数据的指针
+const char* Buffer::Peek() const {
+    
+    return &buffer_[readPos_];
+}
+
+// 确保可写的长度
+void Buffer::EnsureWriteable(size_t len) {
+    if(len > WritableBytes()) {
+        MakeSpace_(len);
+    }
+    assert(len <= WritableBytes());
+}
+
+// 移动写下标，在Append中使用
+void Buffer::HasWritten(size_t len) {
+    writePos_ += len;
+}
+
+// 读取len长度，移动读下标
+void Buffer::Retrieve(size_t len) {
+    readPos_ += len;
+}
+
+// 读取到end位置
+void Buffer::RetrieveUntil(const char* end) {
+    assert(Peek() <= end );
+    Retrieve(end - Peek()); // end指针 - 读指针 长度
+}
+
+// buffer归零，读写下标归零,在别的函数中会用到
+void Buffer::RetrieveAll() {
+    bzero(&buffer_[0], buffer_.size()); // 覆盖原本数据
+    readPos_ = writePos_ = 0;
+}
+
+// 取出剩余可读的str
+std::string Buffer::RetrieveAllToStr() {
+    //使用 Peek() 返回的指针作为数据源的起始位置;
+    //使用 ReadableBytes() 返回的可读数据字节数作为数据的长度;
+    //将这些数据拷贝到一个新创建的 std::string 对象 str 中
+    std::string str(Peek(), ReadableBytes());
+    RetrieveAll();
+    return str;
+}
+
+// 写指针的位置
+// 常量指针，不能用于修改其指向的缓冲区中的数据
+const char* Buffer::BeginWriteConst() const {
+    return &buffer_[writePos_];
+}
+//普通字符型指针，可以用于修改指向的缓冲区中的数据
+char* Buffer::BeginWrite() {
+    return &buffer_[writePos_];
+}
+
+// 添加str到缓冲区
+//-接收字符串常量指针和长度
+void Buffer::Append(const char* str, size_t len) {
+    assert(str);
+    EnsureWriteable(len);   // 确保可写的长度
+    std::copy(str, str + len, BeginWrite());    // 将str放到写下标开始的地方
+    HasWritten(len);    // 移动写下标
+}
+//-接收字符串对象
+void Buffer::Append(const std::string& str) {
+    Append(str.c_str(), str.size());
+}
+//-接收任意数据指针和长度
+void Append(const void* data, size_t len) {
+    Append(static_cast<const char*>(data), len);
+}
+
+// 将buffer中的读下标的地方放到该buffer中的写下标位置
+void Append(const Buffer& buff) {
+    Append(buff.Peek(), buff.ReadableBytes());
+}
+
+// 将fd的内容读到缓冲区，即writable的位置
+ssize_t Buffer::ReadFd(int fd, int* Errno) {
+    char buff[65535];   //在栈区分配一个名为 buff 的字符数组，用于临时存储从文件描述符 fd 中读取的 存不下的 数据。
+    struct iovec iov[2];    //定义一个 iovec 结构的数组 iov，iovec 是分散/聚集输入输出的数据结构。在这里，创建一个包含两个元素的数组，每个元素描述了一个数据块的位置和大小。
+    size_t writeable = WritableBytes(); // 先记录能写多少
+    // 分散读， 保证数据全部读完
+    iov[0].iov_base = BeginWrite();
+    iov[0].iov_len = writeable;
+    iov[1].iov_base = buff;
+    iov[1].iov_len = sizeof(buff);
+
+    ssize_t len = readv(fd, iov, 2);
+    if(len < 0) {
+        *Errno = errno;
+    } else if(static_cast<size_t>(len) <= writeable) {   // 若len小于writable，说明写区可以容纳len
+        writePos_ += len;   // 直接移动写下标
+    } else {    
+        writePos_ = buffer_.size(); // 写区写满了,下标移到最后
+        Append(buff, static_cast<size_t>(len - writeable)); // !剩余的长度，后续怎么处理呢？
+    }
+    return len;
+}
+
+// 将buffer中可读的区域写入fd中
+ssize_t Buffer::WriteFd(int fd, int* Errno) {
+    ssize_t len = write(fd, Peek(), ReadableBytes());
+    // !在buff中的没有写入fd呀？？
+    if(len < 0) {
+        *Errno = errno;
+        return len;
+    } 
+    Retrieve(len);
+    return len;
+}
+
+// 返回缓冲区起始指针
+char* Buffer::BeginPtr_() {
+    return &buffer_[0];
+}
+
+// 返回缓冲区起始常量指针
+const char* Buffer::BeginPtr_() const{
+    return &buffer_[0];
+}
+
+// 扩展空间，确保写入数据时不会造成缓冲区溢出
+void Buffer::MakeSpace_(size_t len) {
+    // 如果可写空间加上预留空间都不足以容纳要写入的数据
+    // 则需要扩展缓冲区的大小
+    if(WritableBytes() + PrependableBytes() < len) {
+        buffer_.resize(writePos_ + len + 1);
+    } else {
+        // 如果可写空间加上预留空间足够容纳要写入的数据
+        // 则可以通过移动数据来腾出足够的可写空间
+        size_t readable = ReadableBytes();
+        //将已读取的数据复制到缓冲区起始位置
+        std::copy(BeginPtr_() + readPos_, BeginPtr_() + writePos_, BeginPtr_());
+        readPos_ = 0;
+        writePos_ = readable;
+        assert(readable == ReadableBytes());
+    }
+}
+
---
+++ code/buffer/buffer.h
@@ -0,0 +1,51 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+#include <cstring>   //perror
+#include <iostream>
+#include <unistd.h>  // write
+#include <sys/uio.h> //readv
+#include <vector> //readv
+#include <atomic>
+#include <assert.h>
+class Buffer {
+public:
+    //构造函数，指定缓冲区的大小，默认1024字节
+    Buffer(int initBuffSize = 1024);
+    ~Buffer() = default;
+
+    size_t WritableBytes() const;       
+    size_t ReadableBytes() const ;
+    size_t PrependableBytes() const;
+
+    const char* Peek() const;
+    void EnsureWriteable(size_t len);
+    void HasWritten(size_t len);
+
+    void Retrieve(size_t len);
+    void RetrieveUntil(const char* end);
+
+    void RetrieveAll();
+    std::string RetrieveAllToStr();
+
+    const char* BeginWriteConst() const;
+    char* BeginWrite();
+
+    void Append(const std::string& str);
+    void Append(const char* str, size_t len);
+    void Append(const void* data, size_t len);
+    void Append(const Buffer& buff);
+
+    ssize_t ReadFd(int fd, int* Errno);
+    ssize_t WriteFd(int fd, int* Errno);
+
+private:
+    char* BeginPtr_();  // buffer开头
+    const char* BeginPtr_() const;
+    void MakeSpace_(size_t len);
+
+    std::vector<char> buffer_; //char类型大小是1个字节，适合处理字节级数据
+    std::atomic<std::size_t> readPos_;  // 读的下标
+    std::atomic<std::size_t> writePos_; // 写的下标
+};
+
+#endif //BUFFER_H
